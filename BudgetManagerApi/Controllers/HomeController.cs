400253a6280130cf23c0bbc0e0e52daab80b6eb9
Léon Silva Erhard <l.erhard@gmail.com>
2024-07-03 23:53:01 -0300

    ci: converting from minimal api to a controller api

---
+++ BudgetManagerApi/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetManagerApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HomeController:ControllerBase
+{
+   [HttpGet]
+   public async Task<IActionResult> Index()
+   {
+      return Ok(new { message = "It's fine, I swear!" });
+   }
+}
